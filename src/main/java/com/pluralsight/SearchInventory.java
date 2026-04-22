a0ac05e392c8726422e2436293d37fa9f9a0b5d0
christian-nieves <thechristiannieves@gmail.com>
2026-04-22 11:39:46 -0400

    Try Catch added

--- src/main/java/com/pluralsight/SearchInventory.java
+++ src/main/java/com/pluralsight/SearchInventory.java
@@ -1,5 +1,8 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
@@ -12,8 +15,20 @@ public class SearchInventory {
         }
     }
 
-    public static ArrayList<Product> getInventory() {
+    public static ArrayList<Product> getInventory() throws FileNotFoundException {
         ArrayList<Product> inventory = new ArrayList<>();
+
+        String fileName = "inventory.csv";
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+            String line;
+
+
+
+        } catch (Exception ex) {
+            System.out.println("An error has occurred, please try again.");
+        }
 
 
 
