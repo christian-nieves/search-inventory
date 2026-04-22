0c8ab4961978b11b0c46ae3da637f1c50668a195
christian-nieves <thechristiannieves@gmail.com>
2026-04-22 11:17:11 -0400

    Main page started

--- src/main/java/com/pluralsight/SearchInventory.java
+++ src/main/java/com/pluralsight/SearchInventory.java
@@ -1,4 +1,22 @@
 package com.pluralsight;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class SearchInventory {
+    public static void main(String[] args) {
+        ArrayList<Product> inventory = getInventory();
+        for (Product product : inventory) {
+            System.out.println(product);
+
+        }
+    }
+
+    public static ArrayList<Product> getInventory() {
+        ArrayList<Product> inventory = new ArrayList<>();
+
+
+
+        return inventory;
+    }
 }
